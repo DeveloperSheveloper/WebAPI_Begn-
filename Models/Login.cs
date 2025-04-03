e398d608b81c1bed758209bd5033c77ed0755b33
Harsh Raj <harshhraj.dev@gmail.com>
2025-04-03 10:47:42 +0530

    - added proprties to Model Class [Login, User]

--- Models/Login.cs
+++ Models/Login.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,12 @@ namespace LoginAPI.Models
 {
     public class Login
     {
+        public int Id { get; set; }     
+        public string loginType { get; set; }     
+        public string username { get; set; }     
+        public string emailId { get; set; }     
+        public string password { get; set; }     
+        public string lastPassword { get; set; }     
+        public string isActive { get; set; }     
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -7,5 +7,11 @@ namespace LoginAPI.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string loginType { get; set; }
+        public string username { get; set; }
+        public string emailId { get; set; }
+        public string password { get; set; }
+        public string lastPassword { get; set; }
     }
 }

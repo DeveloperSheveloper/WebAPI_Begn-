3ad1b545f7260d11fec8b39cef998b3944d37917
Harsh Raj <harshhraj.dev@gmail.com>
2025-04-04 14:47:25 +0530

    - introduced 'custom' bundle in BundleConfig.cs - Modified  RouteConfig.cs by setting Login as View - Created custom folder in Content file for custom stylesheets

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace LoginAPI
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/custom").Include(
+                     "~/Content/custom/login.css"));
         }
     }
 }
